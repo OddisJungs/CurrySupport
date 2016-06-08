9bf5a775055548ad1f2307806fb142a223a7acb4
Aloisli Bürgisser <BuergisserA@EB.local>
2016-06-08 16:15:01 +0200

    Add Project Data Model

--- CurrySupport/CurrySupport.DataModel/Class1.cs
+++ CurrySupport/CurrySupport.DataModel/Class1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CurrySupport.Model
+namespace CurrySupport.DataModel
 {
-    public class CurrySupportDBContext
+    public class Class1
     {
     }
 }
