97595fcdf65a69f6661f0cc20ac83e5fcca6ef1b
Aloisli Bürgisser <Aloisli Brgisser>
2016-06-11 17:12:57 +0200

    Seed Method is running

--- CurrySupport/CurrySupport.DataModel/Migrations/Configuration.cs
+++ CurrySupport/CurrySupport.DataModel/Migrations/Configuration.cs
@@ -29,24 +29,25 @@ namespace CurrySupport.DataModel.Migrations
             //
             context.AlleStatusse.AddOrUpdate(x => x.Id,
                 new Status() { Id = 1, Bezeichnung = "Bereit", Beschreibung = "Alles Bereit" });
-
+            context.SaveChanges();
             context.AlleKategorien.AddOrUpdate(x => x.Id,
                 new Kategorie() { Id = 1, Bezeichnung = "System Fehler", Aktiv = true });
-
+            context.SaveChanges();
             context.AlleUnterkategorien.AddOrUpdate(x => x.Id,
                 new Unterkategorie() { Id = 1, Bezeichnung = "Hardware Fehler", Aktiv = true, Kategorie = context.AlleKategorien.FirstOrDefault(x => x.Id == 1) });
-
+            context.SaveChanges();
             context.AlleRollen.AddOrUpdate(x => x.Id,
                 new Rolle() { Id = 1, Bezeichnung = "Bearbeiter" });
-
+            context.SaveChanges();
             context.AlleRollen.AddOrUpdate(x => x.Id,
                 new Rolle() { Id = 2, Bezeichnung = "Kunde" });
-
+            context.SaveChanges();
             context.AllePersonen.AddOrUpdate(x => x.Id,
                 new Person() { Id = 1, Name = "Bürgisser", Vorname = "Reto", Telefonnummer = "0417505234", Adresse = "Eggstrasse 2", Aktiv = true, Rolle = context.AlleRollen.FirstOrDefault(x => x.Id == 1) });
-
+            context.SaveChanges();
             context.AlleTickets.AddOrUpdate(x => x.Id,
                 new Ticket() { Id = 1, Beschreibung = "Dieses Programm fertig schreiben", Priorität = 100, Aenderungsdatum=DateTime.Now, Erstellungsdatum=DateTime.Now, Kategorie = context.AlleKategorien.FirstOrDefault(x => x.Id == 1), Status = context.AlleStatusse.FirstOrDefault(x => x.Id == 1), Unterkategorie = context.AlleUnterkategorien.FirstOrDefault(x => x.Id == 1), Personen= new ObservableCollection<Person>(context.AllePersonen.Where(x => x.Id == 1).ToList()) });
+            context.SaveChanges();
         }
     }
 }
